49866a9c5f22c269fda15afa20b1911b8e359c5e
NicholasDR-UCLL <nicholas.deruyck@student.ucll.be>
2025-11-13 14:38:51 +0100

    js math

--- js/jsbasis.js
+++ js/jsbasis.js
@@ -7,3 +7,25 @@ console.log(kwadraat);
 const tekst = 'hallo';
 
 const waar = true;
+let y = 11;
+
+console.log(x == y);
+console.log(x > y);
+
+console.log("---")
+let tien = "10";
+console.log(x == tien);
+console.log(x === tien);//!!!
+
+console.log("---");
+console.log("appel" < "Appel");
+
+if(x%2 === 0) console.log("even")
+else console.log("oneven");
+
+let result = x%2 === 0 ? "even" : "oneven";
+console.log(result)
+
+let a = 3, b = 4;
+console.log(a === 3 || b === 0);
+console.log(a < 10 && b > 0);
